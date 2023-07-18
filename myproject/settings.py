58c565990a479e2746d5a8cea66de82101cf9305
Enzo Martinez <enzomartinez2412@gmail.com>
2023-07-18 00:27:22 -0500

    commitfinal

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8mau*#txynfuhwwmr6rmr7&do&a+98xeos)wc3j=$&o*pq)#^o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
